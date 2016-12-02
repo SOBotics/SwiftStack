b088300f31c3a7f1515eb2f4325209742ce9ee1a
Felix Deil <felix.deil@student.uni-augsburg.de>
2016-12-02 22:12:30 +0100

    Created the User struct

---
+++ Sources/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  SwiftStack
+//
+//  Created by FelixSFD on 02.12.16.
+//
+//
+
+import Foundation
+
+/**
+ Represents an user on StackExchange.
+ 
+ - author: FelixSFD
+ 
+ - seealso: [StackExchange API](https://api.stackexchange.com/docs/types/user)
+ */
+public struct User {
+    
+}
