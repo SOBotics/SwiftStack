cf4b918555abb5f52ec4fb175d396309433d0061
Felix Deil <felix.deil@student.uni-augsburg.de>
2016-12-03 10:41:57 +0100

    Added `UserType` to the user-struct/class

--- Sources/User.swift
+++ Sources/User.swift
@@ -17,4 +17,44 @@ import Foundation
  */
 public struct User {
     
+    // - MARK: The user-type
+    
+    /**
+     The tpye of the user. The StackExchange API returns different types of users depending on the use-case.
+     
+     - note: `Type` would be swiftier, but it's a reserved keyword and doesn't work properly
+     
+     - author: FelixSFD
+     */
+    public enum UserType: String {
+        /**
+         This user-type can contain all available properties
+         */
+        case full = "full_user"
+        case shallow = "shallow_user"
+        case network = "network_user"
+        
+        /**
+         This user-type could contain anything, but most likely, it is not initialized correctly.
+         */
+        case undefined = "undefined"
+        
+        public init() {
+            self = .undefined
+        }
+    }
+    
+    // - MARK: Public properties
+    
+    /**
+     The `User.UserType` of the user.
+     
+     This does not affect the other properties. It's just for the developers to see, which data they can expect.
+     */
+    public var type = User.UserType.undefined
+    
+    
+    
+    // - MARK: Values returned from API
+    
 }
