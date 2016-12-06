d56a4b93241686c446dc5454b1a5123b7ab9fa41
FelixSFD <felix.deil@student.uni-augsburg.de>
2016-12-06 17:20:15 +0100

    + improved documentation of the JsonConvertible protocol

--- Sources/JsonConvertible.swift
+++ Sources/JsonConvertible.swift
@@ -11,6 +11,8 @@ import Foundation
 /**
  Objects that can be created fom a JSON-string or be converted to one, conform to this protocol.
  
+ The used datatypes should be in the format as returned from the StackExchange API. (for example `Date` as UNIX timestamp)
+ 
  - note: This protocol inherits from `DictionaryConvertible`
  
  - author: FelixSFD
