4e31725d7bb6cd2bd9516fe22c0fd4e04d5f0e01
FelixSFD <felix.deil@student.uni-augsburg.de>
2016-12-07 09:13:12 +0100

    + added dictionary initializer for Question

--- Sources/Question.swift
+++ Sources/Question.swift
@@ -13,7 +13,62 @@ public class Question: Post {
     
     // - MARK: Initializers
     
-    
+    public override init(dictionary: [String: Any]) {
+        super.init(dictionary: dictionary)
+        
+        //only set the new properties here. The rest is managed by the superclass
+        
+        self.accepted_answer_id = dictionary["accepted_answer_id"] as? Int
+        self.answer_count = dictionary["answer_count"] as? Int
+        //self.answers = nil
+        self.bounty_amount = dictionary["bounty_amount"] as? Int
+        
+        if let timestamp = dictionary["bounty_closes_date"] as? Double {
+            self.bounty_closes_date = Date(timeIntervalSince1970: timestamp)
+        }
+        
+        //self.bounty_user = nil
+        self.can_close = dictionary["can_close"] as? Bool
+        self.can_flag = dictionary["can_flag"] as? Bool
+        self.close_vote_count = dictionary["close_vote_count"] as? Int
+        
+        if let timestamp = dictionary["closed_date"] as? Double {
+            self.closed_date = Date(timeIntervalSince1970: timestamp)
+        }
+        
+        //self.closed_details = nil
+        self.closed_reason = dictionary["closed_reason"] as? String
+        
+        if let timestamp = dictionary["community_owned_date"] as? Double {
+            self.community_owned_date = Date(timeIntervalSince1970: timestamp)
+        }
+        
+        if let timestamp = dictionary["creation_date"] as? Double {
+            self.creation_date = Date(timeIntervalSince1970: timestamp)
+        }
+        
+        self.delete_vote_count = dictionary["delete_vote_count"] as? Int
+        self.favorite_count = dictionary["favorite_count"] as? Int
+        self.favorited = dictionary["favorites"] as? Bool
+        self.is_answered = dictionary["is_answered"] as? Bool
+        
+        if let timestamp = dictionary["locked_date"] as? Double {
+            self.locked_date = Date(timeIntervalSince1970: timestamp)
+        }
+        
+        //self.migrated_from = nil
+        //self.migrated_to = nil
+        //self.notice = nil
+        
+        if let timestamp = dictionary["protected_date"] as? Double {
+            self.protected_date = Date(timeIntervalSince1970: timestamp)
+        }
+        
+        self.question_id = dictionary["question_id"] as? Int
+        self.reopen_vote_count = dictionary["reopen_vote_count"] as? Int
+        //self.tags = nil
+        self.view_count = dictionary["view_count"] as? Int
+    }
     
     // - MARK: Properties returned from API
     
