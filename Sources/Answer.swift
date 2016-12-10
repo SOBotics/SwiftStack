2fbd60cbe7303b9fa21fd8e701132a39cbb05efd
FelixSFD <felix.deil@student.uni-augsburg.de>
2016-12-10 09:53:07 +0100

    + added the Answer class and a basic test for it

---
+++ Sources/Answer.swift
@@ -0,0 +1,90 @@
+//
+//  Answer.swift
+//  SwiftStack
+//
+//  Created by FelixSFD on 09.12.16.
+//
+//
+
+import Foundation
+
+public class Answer: Post {
+    
+    // - Initializers
+    
+    public required init(dictionary: [String : Any]) {
+        super.init(dictionary: dictionary)
+        
+        self.accepted = dictionary["accepted"] as? Bool
+        self.answer_id = dictionary["answer_id"] as? Int
+        self.awarded_bounty_amount = dictionary["awarded_bounty_amount"] as? Int
+        
+        if let users = dictionary["awarded_bounty_users"] as? [[String: Any]] {
+            var array = [User]()
+            
+            for user in users {
+                let tmpUser = User(dictionary: user)
+                array.append(tmpUser)
+            }
+            
+            if array.count > 0 {
+                self.awarded_bounty_users = array
+            }
+        }
+        
+        self.can_flag = dictionary["can_flag"] as? Bool
+        
+        if let timestamp = dictionary["community_owned_cate"] as? Double {
+            self.community_owned_date = Date(timeIntervalSince1970: timestamp)
+        }
+        
+        if let timestamp = dictionary["creation_date"] as? Double {
+            self.creation_date = Date(timeIntervalSince1970: timestamp)
+        }
+        
+        self.is_accepted = dictionary["is_accepted"] as? Bool
+        
+        if let timestamp = dictionary["locked_date"] as? Double {
+            self.locked_date = Date(timeIntervalSince1970: timestamp)
+        }
+        
+        self.tags = dictionary["tags"] as? [String]
+    }
+    
+    public required convenience init?(jsonString json: String) {
+        do {
+            guard let dictionary = try JSONSerialization.jsonObject(with: json.data(using: String.Encoding.utf8)!, options: .allowFragments) as? [String: Any] else {
+                return nil
+            }
+            
+            self.init(dictionary: dictionary)
+        } catch {
+            return nil
+        }
+    }
+    
+    // - MARK: Fields
+    
+    public var accepted: Bool?
+    
+    public var answer_id: Int?
+    
+    public var awarded_bounty_amount: Int?
+    
+    public var awarded_bounty_users: [User]?
+    
+    public var can_flag: Bool?
+    
+    public var community_owned_date: Date?
+    
+    public var creation_date: Date?
+    
+    public var is_accepted: Bool?
+    
+    public var locked_date: Date?
+    
+    public var question_id: Int?
+    
+    public var tags: [String]?
+    
+}
--- Tests/SwiftStackTests/PostTests.swift
+++ Tests/SwiftStackTests/PostTests.swift
@@ -20,4 +20,13 @@ class PostTests: XCTestCase {
         XCTAssertNotNil(jsonQuestion)
     }
     
+    func testBasicAnswer() {
+        let json = "{\"owner\": {\"reputation\": 9001,\"user_id\": 1,\"user_type\": \"registered\",\"accept_rate\": 55,\"profile_image\": \"https://www.gravatar.com/avatar/a007be5a61f6aa8f3e85ae2fc18dd66e?d=identicon&r=PG\",\"display_name\": \"Example User\",\"link\": \"http://example.stackexchange.com/users/1/example-user\"},\"down_vote_count\": 2,\"up_vote_count\": 3,\"is_accepted\": false,\"score\": 1,\"last_activity_date\": 1481316638,\"creation_date\": 1481273438,\"locked_date\": 1481338238,\"answer_id\": 5678,\"question_id\": 1234,\"link\": \"http://example.stackexchange.com/questions/1234/an-example-post-title/5678#5678\",\"title\": \"An example post title\",\"body\": \"An example post body\"}"
+        
+        let answer = Answer(jsonString: json)
+        let jsonAnswer = answer?.jsonString
+        print(jsonAnswer)
+        XCTAssertNotNil(jsonAnswer)
+    }
+    
 }
