7b70b5597c950b304bda85c20737363d13183490
Jonathan Keller <jkeller234@icloud.com>
2016-12-18 20:54:16 -0800

    fix incorrect module name

--- build-nopm.sh
+++ build-nopm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Generating library..."
-swiftc -emit-library -emit-object -module-name SwiftChatSE Sources/*.swift || exit 1
-ar rcs libSwiftChatSE.a *.o || exit 1
+swiftc -emit-library -emit-object -module-name SwiftStack Sources/*.swift || exit 1
+ar rcs libSwiftStack.a *.o || exit 1
 rm *.o
 
 echo "Generating swiftmodule..."
-swiftc -emit-module -module-name SwiftChatSE Sources/*.swift || exit 1
+swiftc -emit-module -module-name SwiftStack Sources/*.swift || exit 1
