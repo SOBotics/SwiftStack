81af352069afaf121382da148cab24fc3aa787d8
FelixSFD <felix.deil@student.uni-augsburg.de>
2017-01-04 15:07:04 +0100

    + added requests for /questions/no-answers

--- Sources/RequestsQuestions.swift
+++ Sources/RequestsQuestions.swift
@@ -655,5 +655,60 @@ public extension APIClient {
             }
         }
     }
+    
+    
+    
+    // - MARK: /questions/no-answers
+    /**
+     Fetches all questions with no answers on the site synchronously.
+     
+     - parameter parameters: The dictionary of parameters used in the request
+     
+     - parameter backoffBehavior: The behavior when an `APIRequest` has a backoff
+     
+     - returns: The list of questions as `APIResponse<Question>`
+     
+     - author: FelixSFD
+     */
+    public func fetchQuestionsWithNoAnswers(
+        parameters: [String:String] = [:],
+        backoffBehavior: BackoffBehavior = .wait) throws -> APIResponse<Question> {
+        
+        return try performAPIRequest(
+            "questions/no-answers",
+            parameters: parameters,
+            backoffBehavior: backoffBehavior
+        )
+    }
+    
+    /**
+     Fetches all questions with no answers on the site asynchronously.
+     
+     - parameter parameters: The dictionary of parameters used in the request
+     
+     - parameter backoffBehavior: The behavior when an `APIRequest` has a backoff
+     
+     - parameter completion
+     
+     - author: FelixSFD
+     */
+    public func fetchQuestionsWithNoAnswers(
+        parameters: [String: String] = [:],
+        backoffBehavior: BackoffBehavior = .wait,
+        completionHandler: @escaping (APIResponse<Question>?, Error?) -> ()) {
+        
+        queue.async {
+            do {
+                let response: APIResponse<Question> = try self.fetchQuestions(
+                    parameters: parameters,
+                    backoffBehavior: backoffBehavior
+                )
+                
+                completionHandler(response, nil)
+            } catch {
+                completionHandler(nil, error)
+            }
+        }
+    }
 	
 }
