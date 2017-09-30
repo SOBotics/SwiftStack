da85d2fc46f5684c17e4a0d604a874ae8ef8a8ff
Jonathan Keller <jkeller234@icloud.com>
2017-09-29 21:19:41 -0700

    Add libary product to Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,10 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftStack",
+    products: [
+        .library(name: "SwiftStack", targets: ["SwiftStack"]),
+    ],
+                    
     targets: [
         .target(name: "SwiftStack"),
         .testTarget(name: "SwiftStackTests", dependencies: ["SwiftStack"])
